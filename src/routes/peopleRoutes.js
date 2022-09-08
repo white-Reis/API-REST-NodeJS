7075a4cdaf25376bcabe4b5b314eadf31202f82b
Fabio Reis <82837278+white-Reis@users.noreply.github.com>
2022-09-08 15:23:17 -0300

    refactoring commit
    correction of routes... sorry alex

--- src/routes/peopleRoutes.js
+++ src/routes/peopleRoutes.js
@@ -4,11 +4,11 @@ import peopleController from '../controllers/peoplesControl.js'
 const router = express.Router()
 
 router
-    .get('/people', peopleController.listPeoples)
-    .get('/people/:name', peopleController.listPeopleForName)
-    .get('/people/:id', peopleController.listPeopleForId)
-    .post('/people', peopleController.createPeople)
-    .put('/people/:id', peopleController.attPeople)
-    .delete('/people/:id', peopleController.deletePeople)
+  .get('/api/v1/users', peopleController.listPeoples)
+  .get('/api/v1/users/search/:name', peopleController.listPeopleForName)
+  .get('/api/v1/users/:id', peopleController.listPeopleForId)
+  .post('/api/v1/users', peopleController.createPeople)
+  .put('/api/v1/users/:id', peopleController.attPeople)
+  .delete('/api/v1/users/:id', peopleController.deletePeople)
 
 export default router
